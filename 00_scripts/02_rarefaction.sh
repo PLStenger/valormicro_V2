5ed3323ca439ff8ac20f20335a5497aea8e9b04d
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2026-08-31 10:27:18 +0200

    Create 02_rarefaction.sh

---
+++ 00_scripts/02_rarefaction.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+WORKING_DIRECTORY=/nvme/bio/data_fungi/valormicro_V2/02_amplicon_pipeline/05_qiime2
+DATABASE=/nvme/bio/data_fungi/valormicro_V2/02_amplicon_pipeline/04_database_files
+
+# Aim: rarefy a feature table to compare alpha/beta diversity results
+# A good forum to understand what it does :
+# https://forum.qiime2.org/t/can-someone-help-in-alpha-rarefaction-plotting-depths/4580/16
+
+cd $WORKING_DIRECTORY
+
+eval "$(conda shell.bash hook)"
+conda activate qiime2-amplicon-2025.7
+
+qiime diversity alpha-rarefaction \
+  --i-table core/Table.qza \
+  --i-phylogeny tree/rooted-tree.qza \
+  --p-max-depth 15959 \
+  --p-min-depth 1 \
+  --m-metadata-file $DATABASE/sample-metadata.tsv \
+  --o-visualization visual/alpha-rarefaction.qzv
